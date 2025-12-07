cc417a428bccd69e9784b46fb6c6e64368820928
Muhammad Eid <muhamad.iyd1@ogr.sakarya.edu.tr>
2025-12-07 17:13:04 +0300

    Add missing using directive in CoachController for Rendering

--- Controllers/CoachController.cs
+++ Controllers/CoachController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace FitnessCenter.Controllers
 {
     [Authorize(Roles = "admin")]
