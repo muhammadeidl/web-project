b34e56341bd95078aeac3f2781ef2ef23a4e656b
Muhammad Eid <muhamad.iyd1@ogr.sakarya.edu.tr>
2025-12-15 22:40:45 +0300

    Make Name and Expertise properties nullable
    Updated properties in Coach class to be nullable.

--- Models/Coach.cs
+++ Models/Coach.cs
@@ -1,15 +1,17 @@
-﻿namespace FitnessCenter.Models
- {
+namespace FitnessCenter.Models
+{
     public class Coach
     {
         public int CoachId { get; set; }
-        public string Name { get; set; }
-        public string Expertise { get; set; } 
+        public string? Name { get; set; }
+        public string? Expertise { get; set; } 
 
         public int GymId { get; set; }
         public Gym? Gym { get; set; }
+        public ICollection<CoachTrainingProgram> CoachTrainingPrograms { get; set; } = new List<CoachTrainingProgram>();
 
         public ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
         public ICollection<CoachAvailability> CoachAvailabilities { get; set; } = new List<CoachAvailability>();
+        public ICollection<UnavailableSlot> UnavailableSlots { get; set; } = new List<UnavailableSlot>();
     }
 }
